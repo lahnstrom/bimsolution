994d43c5eac7dfbef4d936907580944089ff5ae8
Love Ahnström <love.ahnstrom@gmail.com>
2018-09-03 11:29:56 +0200

    Experiment with ServiceInterface

--- src/se/bimsolution/Main.java
+++ src/se/bimsolution/Main.java
@@ -2,8 +2,16 @@ package se.bimsolution;
 
 import org.bimserver.client.BimServerClient;
 import org.bimserver.client.json.JsonBimServerClientFactory;
+import org.bimserver.emf.IfcModelInterface;
+import org.bimserver.interfaces.objects.SDataObject;
+import org.bimserver.interfaces.objects.SProject;
+import org.bimserver.models.ifc4.IfcDoor;
+import org.bimserver.models.ifc4.IfcDoorStandardCase;
+import org.bimserver.models.ifc4.IfcRelDefinesByProperties;
 import org.bimserver.shared.UsernamePasswordAuthenticationInfo;
 import org.bimserver.shared.exceptions.BimServerClientException;
+import org.bimserver.shared.meta.SClass;
+import org.bimserver.shared.meta.SField;
 
 /**
  * Handles the queries to the BIMServer
@@ -13,7 +21,18 @@ public class Main {
         try (JsonBimServerClientFactory factory = new JsonBimServerClientFactory("http://104.248.40.190:8080/bimserver")) {
             // Creating a client in a try statement, this makes sure the client will be closed after use
             try (BimServerClient client = factory.create(new UsernamePasswordAuthenticationInfo(args[0], args[1]))) {
-                client.getServiceInterface().getAllProjects(false, false).forEach(x -> System.out.println(x.getName()));
+                SProject project = client.getServiceInterface().getProjectsByName("A2-400").get(0);
+                System.out.println(project.getOid());
+                IfcModelInterface modelInterface = client.getModel(project, project.getLastRevisionId(), true, false, false);
+                System.out.println("Im here: " + modelInterface);
+
+                System.out.println(modelInterface);
+//
+                modelInterface.getAllWithSubTypes(IfcDoor.class);
+//                HashSet<>
+//               client.getServiceInterface().download()
+//                SDataObject sDataObject = client.getLowLevelInterface().getDataObjectByOid(project.getLastRevisionId(), 393834L);
+//                sDataObject.getValues().forEach(x-> System.out.println(x));
             } catch (Exception e) {
                 e.printStackTrace();
             }
