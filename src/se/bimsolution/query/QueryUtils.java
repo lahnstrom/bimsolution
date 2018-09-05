29d9d4ecabc196078841cb089b740b593026cda5
Love Ahnström <love.ahnstrom@gmail.com>
2018-09-05 13:20:19 +0200

    Refactor the classlist in IdValidationMachine

--- src/se/bimsolution/query/QueryUtils.java
+++ src/se/bimsolution/query/QueryUtils.java
@@ -1,5 +1,7 @@
 package se.bimsolution.query;
 
+import org.bimserver.models.ifc2x3tc1.*;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -16,7 +18,63 @@ public final class QueryUtils {
 
     private QueryUtils() {}
 
-
+    /**
+     * The classes we're checking in the Query Machines
+     * @return a list of classes
+     */
+    public static List<Class> standardClassList() {
+        List<Class> classList = new ArrayList<>();
+        classList.add(IfcDoor.class);
+        classList.add(IfcFlowSegment.class);
+//        classList.add(IfcFurnitureType.class);
+        classList.add(IfcAirTerminalType.class);
+        classList.add(IfcAirToAirHeatRecoveryType.class);
+        classList.add(IfcBeam.class);
+        classList.add(IfcBuilding.class);
+        classList.add(IfcCompressorType.class);
+//        classList.add(IfcBuildingStorey.class);
+        classList.add(IfcColumn.class);
+        classList.add(IfcCovering.class);
+        classList.add(IfcCurtainWall.class);
+        classList.add(IfcAirTerminalType.class);
+        classList.add(IfcAirTerminalBoxType.class);
+        classList.add(IfcDuctSegmentType.class);
+        classList.add(IfcDuctFittingType.class);
+        classList.add(IfcDuctSilencerType.class);
+        classList.add(IfcEquipmentElement.class);
+        classList.add(IfcFireSuppressionTerminalType.class);
+        classList.add(IfcFanType.class);
+        classList.add(IfcFilterType.class);
+        classList.add(IfcTankType.class);
+        classList.add(IfcFlowTerminal.class);
+        classList.add(IfcFooting.class);
+        classList.add(IfcHumidifierType.class);
+        classList.add(IfcUnitaryEquipmentType.class);
+        classList.add(IfcPile.class);
+        classList.add(IfcPumpType.class);
+        classList.add(IfcPipeFittingType.class);
+        classList.add(IfcSwitchingDeviceType.class);
+        classList.add(IfcPipeSegmentType.class);
+        classList.add(IfcRamp.class);
+        classList.add(IfcRampFlight.class);
+        classList.add(IfcStairFlight.class);
+        classList.add(IfcStair.class);
+//        classList.add(IfcSlab.class);
+        classList.add(IfcDamperType.class);
+        classList.add(IfcRoof.class);
+        classList.add(IfcSite.class);
+        classList.add(IfcJunctionBoxType.class);
+//        classList.add(IfcSpace.class);
+        classList.add(IfcOutletType.class);
+        classList.add(IfcSystem.class);
+        classList.add(IfcValveType.class);
+        classList.add(IfcWall.class);
+        classList.add(IfcFlowController.class);
+        classList.add(IfcLightFixtureType.class);
+//        classList.add(IfcWindow.class);
+//        classList.add(IfcOpeningElement.class);
+        return classList;
+    }
     public static HashMap<String, HashSet<String>> idToIfcCSVParser (String CSVfilepath, String delimiter) throws IOException {
         String line;
         HashMap<String, HashSet<String>> parsedData = new HashMap<>();
