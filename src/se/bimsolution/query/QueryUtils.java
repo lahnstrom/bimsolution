bc2e6d528ad0cf2ac5e993a4408f8cd763e20bde
Love Ahnström <love.ahnstrom@gmail.com>
2018-09-06 13:40:42 +0200

    Implement getPropertyByName and getSingleValueByName

--- src/se/bimsolution/query/QueryUtils.java
+++ src/se/bimsolution/query/QueryUtils.java
@@ -2,7 +2,9 @@ package se.bimsolution.query;
 
 import org.bimserver.emf.IdEObject;
 import org.bimserver.models.ifc2x3tc1.*;
+import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.util.EContentsEList;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -125,12 +127,56 @@ public final class QueryUtils {
 
     }
 
-    //TODO IfcPset from IfcObject
-    public IfcPropertySet ifcPropertySetFromElement(IfcElement element) {
+    //TODO IfcPsets from IfcObject
+    public EList<IfcPropertySet> ifcPropertySetFromElement(IfcObject element) {
+        EList<IfcRelDefines> definesList = element.getIsDefinedBy();
+        EList<IfcPropertySet> psets = new BasicEList<>();
+        for (IfcRelDefines rel:
+             definesList) {
+            if (rel instanceof IfcRelDefinesByProperties) {
+                IfcPropertySetDefinition pset = ((IfcRelDefinesByProperties) rel).getRelatingPropertyDefinition();
+                if (pset instanceof IfcPropertySet) {
+                    psets.add((IfcPropertySet) pset);
+                }
+            }
+        }
+        if (psets.size()==0) {
+            throw new IllegalArgumentException("The element has no relating IfcPropertySet");
+        }
+        return psets;
+    }
 
+    public IfcPropertySet getPropertySetByName(List<IfcPropertySet> psets, String name) {
+        for (IfcPropertySet pset:
+             psets) {
+            if (pset.getName().equals(name)) {
+                return pset;
+            }
+        }
+        throw new IllegalArgumentException("The property set with name " + name + " does not exist");
+    }
+
+    public IfcPropertySet getPropertySetByStartsWith(List<IfcPropertySet> propertySets ,String startsWith) {
+        for (IfcPropertySet pset:
+                propertySets) {
+            if (pset.getName().startsWith(startsWith)) {
+                return pset;
+            }
+        }
+        throw new IllegalArgumentException("No property starting with " + startsWith + " exist");
     }
 
 
+    public IfcPropertySingleValue getSingleValueByName(IfcPropertySet ifcPropertySet, String name) {
+        EList<IfcProperty> properties = ifcPropertySet.getHasProperties();
+        for (IfcProperty prop:
+             properties) {
+            if (prop instanceof IfcPropertySingleValue && prop.getName().equals(name)) {
+                return (IfcPropertySingleValue) prop;
+            }
+        }
+        throw new IllegalArgumentException("No IfcProperty found in the IfcPropertySet with the name: " + name);
+    }
     //TODO Property with name NAME from pset:
 
     //TODO BSAB96BD from pset
