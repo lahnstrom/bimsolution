0ca60ca980eb545a08fffb6d51fe9969e83bfe24
lahnstrom <love.ahnstrom@gmail.com>
2018-09-10 11:39:39 +0200

    Implement and document run method in QueryCoordinator

--- src/se/bimsolution/query/QueryCoordinator.java
+++ src/se/bimsolution/query/QueryCoordinator.java
@@ -15,31 +15,55 @@ public class QueryCoordinator implements Runnable {
     private int revisionId;
     Repository repo;
     private IfcModelInterface model;
-    List<ElementChecker> checkers;
+    List<Class> classList;
+    HashMap<ElementChecker, Integer> elementCheckerErrorIdMap;
 
-    public QueryCoordinator(int revisionId, Repository repo, IfcModelInterface model, ElementChecker... elementCheckers) {
+
+    public QueryCoordinator(int revisionId, Repository repo, IfcModelInterface model, HashMap<ElementChecker, Integer> elementCheckerErrorIdMap) {
         this.revisionId = revisionId;
         this.repo = repo;
         this.model = model;
-        checkers = Arrays.asList(elementCheckers);
+        this.classList = standardClassList();
+        this.elementCheckerErrorIdMap = elementCheckerErrorIdMap;
     }
 
 
+    public QueryCoordinator(int revisionId, Repository repo, IfcModelInterface model) {
+        this.revisionId = revisionId;
+        this.repo = repo;
+        this.model = model;
+        this.classList = standardClassList();
+        this.elementCheckerErrorIdMap = ElementCheckerUtils.standardElementCheckerMapping();
+    }
+
+    /**
+     * Runs this queryCoordinator on all the objects defined by classes in the classlist.
+     * The method begins by writing all the propertySets to db and returns a map of element-id of pset.
+     * It then fetches all the ifcTypes in the database and creates a map of name- id
+     * Finally, it calls on the checkAllElementsInHashMap Method and writes the fails to DB
+     */
     @Override
     public void run() {
-        List<Class> classList = standardClassList();
         List<IfcElement> elements = new ArrayList<>();
-        for (Class clazz:
-             classList) {
+        for (Class clazz :
+                this.classList) {
             elements.addAll(model.getAll(clazz));
         }
-        HashMap<IfcElement, PropertySet> elementPropertySetHashMap = newIfcElementToAHPropertySetMap(elements);
-        HashMap<IfcElement, Integer> elementIdMap;
+
         try {
-            elementIdMap = repo.writePropertySetsReturnsMap(elementPropertySetHashMap);
+            HashMap<IfcElement, PropertySet> elementPropertySetHashMap = newIfcElementToAHPropertySetMap(elements);
+            HashMap<IfcElement, Integer> elementIdMap = repo.writePropertySetsReturnsMap(elementPropertySetHashMap);
+            HashMap<String, Integer> ifcTypeNameIdMap = repo.getIfcTypeNameIdMap();
+            List<Fail> fails = checkAllElementsInHashMap(this.revisionId,
+                    elementIdMap, ifcTypeNameIdMap, this.elementCheckerErrorIdMap);
+            repo.writeAllFails(fails);
         } catch (SQLException e) {
-            e.printStackTrace();
+            try {
+                repo.writeLog(new Log("Query failed: " + e.getMessage(), this.revisionId));
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
         }
-//        checkAllElementsInHashMap()
+
     }
 }
