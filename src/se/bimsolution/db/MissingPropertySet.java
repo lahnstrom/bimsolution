4c4cccd37d3e770dcd66361e885f9551d915eb68
Love Ahnström <love.ahnstrom@gmail.com>
2018-09-11 13:22:04 +0200

    Add repo method for writing the new objects

--- src/se/bimsolution/db/MissingPropertySet.java
+++ src/se/bimsolution/db/MissingPropertySet.java
@@ -5,17 +5,24 @@ public class MissingPropertySet {
     private long objectId;
     private String ifcBuilding;
     private String ifcStorey;
+    private String ifcSite;
     private String ifcType;
     private int revisionId;
     private String name;
 
-    public MissingPropertySet(long objectId, String ifcBuilding, String ifcStorey, String ifcType, int revisionId, String name) {
+    public MissingPropertySet(long objectId, String ifcBuilding, String ifcStorey, String ifcSite, String ifcType, int revisionId, String name) {
         this.objectId = objectId;
         this.ifcBuilding = ifcBuilding;
         this.ifcStorey = ifcStorey;
+        this.ifcSite = ifcSite;
         this.ifcType = ifcType;
         this.revisionId = revisionId;
         this.name = name;
+    }
+
+
+    public String getIfcSite() {
+        return ifcSite;
     }
 
     public long getObjectId() {
